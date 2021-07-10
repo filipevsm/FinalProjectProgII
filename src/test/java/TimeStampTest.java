171d11b619858b05849a0e1454550642c8a5624a
darbycoin <darbycoin@gmail.com>
2021-07-10 10:30:54 -0400

    done up till add methods

--- src/test/java/TimeStampTest.java
+++ src/test/java/TimeStampTest.java
@@ -6,53 +6,59 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TimeStampTest {
     @Test
     public void testSetHour() {
-        TimeStamp t = new TimeStamp(1,11,11);
+        TimeStamp t = new TimeStamp(1,1,1);
         t.setHour(1);
         assertEquals(1, t.getHour());
     }
 
     @Test
     public void testGetHour() {
-        Employee e = new Employee(123456, "Meghrig", "Terzian", 25.5);
-        assertEquals("Meghrig", e.getFirstName());
+        TimeStamp t = new TimeStamp(1,1,1);
+        assertEquals(1, t.getHour());
     }
 
     @Test
     public void testSetSec() {
-        Employee e = new Employee(123456, "Meghrig", "Terzian", 25.5);
-        assertEquals("Meghrig", e.getFirstName());
+        TimeStamp t = new TimeStamp(1,1,1);
+        t.setSec(1);
+        assertEquals(1, t.getSec());
     }
 
     @Test
     public void testGetSec() {
-        Employee e = new Employee(123456, "Meghrig", "Terzian", 25.5);
-        assertEquals("Meghrig", e.getFirstName());
+        TimeStamp t = new TimeStamp(1,1,1);
+        assertEquals(1, t.getSec());
     }
 
     @Test
     public void testSetMin() {
-        Employee e = new Employee(123456, "Meghrig", "Terzian", 25.5);
-        assertEquals("Meghrig", e.getFirstName());
+        TimeStamp t = new TimeStamp(1,1,1);
+        t.setMin(1);
+        assertEquals(1, t.getMin());
     }
 
     @Test
     public void testGetMin() {
-        Employee e = new Employee(123456, "Meghrig", "Terzian", 25.5);
-        assertEquals("Meghrig", e.getFirstName());
+        TimeStamp t = new TimeStamp(1,1,1);
+        assertEquals(1, t.getHour());
     }
+    // done up to here
     @Test
     public void testAddMin() {
-        Employee e = new Employee(123456, "Meghrig", "Terzian", 25.5);
-        assertEquals("Meghrig", e.getFirstName());
+        TimeStamp t = new TimeStamp(1,1,1);
+        t.setHour(1);
+        assertEquals(1, t.getHour());
     }
     @Test
     public void testAddHour() {
-        Employee e = new Employee(123456, "Meghrig", "Terzian", 25.5);
-        assertEquals("Meghrig", e.getFirstName());
+        TimeStamp t = new TimeStamp(1,1,1);
+        t.setHour(1);
+        assertEquals(1, t.getHour());
     }
     @Test
     public void testAddSec() {
-        Employee e = new Employee(123456, "Meghrig", "Terzian", 25.5);
-        assertEquals("Meghrig", e.getFirstName());
+        TimeStamp t = new TimeStamp(1,1,1);
+        t.setHour(1);
+        assertEquals(1, t.getHour());
     }
 }
